ead56811c1d1563529314d7835790768bfc177ca
subhamsrkr07 <121615827+subhamsrkr07@users.noreply.github.com>
2025-08-04 16:03:34 +0530

    Update index.js

--- Backend/index.js
+++ Backend/index.js
@@ -15,7 +15,7 @@ app.use(express.json())
 app.use(cookieParser())
 app.use(cors(   
     {
-        origin:"https://e-commerece-admin.onrender.com",
+        origin:["https://e-commerece-admin.onrender.com","https://e-commerece-frontend-hhi2.onrender.com"],
         credentials:true
     }
 ))
