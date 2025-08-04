0b9e339ffc35f046217da0616d7fe2b24120c098
subhamsrkr07 <121615827+subhamsrkr07@users.noreply.github.com>
2025-08-04 14:14:07 +0530

    Update Authcontext.jsx

--- Admin/src/context/Authcontext.jsx
+++ Admin/src/context/Authcontext.jsx
@@ -4,7 +4,7 @@ import { createContext } from 'react'
 
 export const authDataContext = createContext()
 const Authcontext = ({children}) => {
-    let serverUrl = "http://localhost:8000"
+    let serverUrl = "https://e-commerece-backend-juxp.onrender.com"
 
 
     let value = {
