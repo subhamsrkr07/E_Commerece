83f6ec3dca1d206b27cac0a43aa65fae566c9c75
subhamsrkr07 <121615827+subhamsrkr07@users.noreply.github.com>
2025-08-05 14:48:02 +0530

    Update UserContext.jsx

--- Frontend/src/context/UserContext.jsx
+++ Frontend/src/context/UserContext.jsx
@@ -6,7 +6,7 @@ import axios from 'axios'
 
 export const userDataContext = createContext()
 function UserContext({children}){
-    let [userData ,setUserData]=useState("")
+    let [userData ,setUserData]=useState(null)
     let {serverUrl}=useContext(authDataContext)
       
     const getCurrentUser = async()=>{
