abc59fe40b969724f9379ac5ea4199cb423f0208
subhamsrkr07 <121615827+subhamsrkr07@users.noreply.github.com>
2025-08-05 16:58:27 +0530

    Update isAuth.js

--- Backend/middleware/isAuth.js
+++ Backend/middleware/isAuth.js
@@ -4,7 +4,6 @@ import jwt from 'jsonwebtoken'
 const isAuth = async (req, res, next) => {
     try {
          // return res.status(200).json(req.cookies)
-        console.log("Cookies:", req.cookies);
 
         let { token } = req.cookies
 
