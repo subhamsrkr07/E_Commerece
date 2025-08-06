45aa3234d70fba78d44e45af727a939bc65b85fb
subhamsrkr07 <121615827+subhamsrkr07@users.noreply.github.com>
2025-08-06 18:37:27 +0530

    Update authController.js

--- Backend/controller/authController.js
+++ Backend/controller/authController.js
@@ -71,7 +71,12 @@ export const login = async (req, res) => {
 
 export const logout = async (req, res) => {
     try {
-        res.clearCookie("token")
+        // res.clearCookie("token")
+        res.clearCookie("token", {
+            httpOnly: true,
+            secure: true,
+            sameSite: "None"
+        })
         return res.status(200).json({ messgae: "logout successfully" })
 
     } catch (error) {
